c3a3a13e7e695811463adae5272290c068476b13
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2025-03-27 22:52:46 +0100

    rust guessing game complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,46 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Please input your guess number. You can enter quit to stop the game.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // User can quit the game by typing quit.
+        if guess.trim() == "quit" {
+            println!("Good bye!");
+            break;
+        }
+
+        /* Ignore a non-number input and continue the program instead of crashing. */
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Oops!! Only numbers are allow >>> \n");
+
+                continue
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
